a71333f9084c20fc52f5d684693c6446ef03dbf2
Huy Duong <hnd1g17@soton.ac.uk>
2020-02-11 10:26:02 +0000

    Added demo folder with mathematical operation files

---
+++ demo_remote/maths_operation.py
@@ -0,0 +1,16 @@
+# This is an original file, no changes have been made
+
+
+def add(x, y):
+    """Edit in local demo and then remote demo"""
+    pass
+
+
+def substract(x, y):
+    """Edit in final fast demo"""
+    pass
+
+
+def multiply(x, y):
+    """Edit in branch demo"""
+    pass
